16ea40673ae362ce332ef140a0acb92c8005bde1
Andrew Smith <andrewcs149@gmail.com>
2020-10-01 15:45:05 -0700

    Add NewPost interface and service.

---
+++ Dev-Blog/Dev-Blog/Models/Interfaces/INewPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Blog.Models.Interfaces
+{
+    public interface INewPost
+    {
+        /// <summary>
+        /// Adds a new post to the database
+        /// </summary>
+        /// <param name="post">The new post</param>
+        /// <returns>New post</returns>
+        Task<NewPost> Create(NewPost post);
+    }
+}
---
+++ Dev-Blog/Dev-Blog/Models/Services/NewPostService.cs
@@ -0,0 +1,33 @@
+﻿using Dev_Blog.Data;
+using Dev_Blog.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dev_Blog.Models.Services
+{
+    public class NewPostService : INewPost
+    {
+        private DevBlogDbContext _context;
+
+        public NewPostService(DevBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Adds a new post to the database
+        /// </summary>
+        /// <param name="post">The new post</param>
+        /// <returns>New post</returns>
+        public async Task<NewPost> Create(NewPost post)
+        {
+            post.Date = DateTime.Now;
+            _context.Entry(post).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return post;
+        }
+    }
+}
