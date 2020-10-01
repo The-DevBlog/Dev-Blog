fbded20788b774d2cc57f78d14c70ffd6df225d5
Andrew Smith <andrewcs149@gmail.com>
2020-10-01 15:03:57 -0700

    Add check mark toggler.

---
+++ Dev-Blog/Dev-Blog/wwwroot/js/Add.js
@@ -0,0 +1,12 @@
+﻿//TODO: not working
+function greenCheck() {
+    var check = document.getElementById("check");
+    var cross = document.getElementById("cross");
+    if (check.style.display === "none") {
+        check.style.display = "block";
+        cross.style.display = "none";
+    } else {
+        check.style.display = "none";
+        cross.style.display = "block";
+    }
+}
