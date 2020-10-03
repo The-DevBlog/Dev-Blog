5c685793c61835f6567b65a6096f9fc77e09436e
Andrew Smith <andrewcs149@gmail.com>
2020-10-03 01:05:17 -0700

    Configure Identity.

---
+++ Dev-Blog/Dev-Blog/Data/UserDevBlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Blog.Data
+{
+    public class UserDevBlogDbContext : DbContext
+    {
+        public UserDevBlogDbContext(DbContextOptions<UserDevBlogDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Dev-Blog/Dev-Blog/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Blog.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
