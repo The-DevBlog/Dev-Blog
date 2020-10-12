3f50b6e6cf328502b58aba7d6704be5694d0dd06
Andrew Smith <andrewcs149@gmail.com>
2020-10-12 14:15:17 -0700

    Delete blob references (no longer using).

--- Dev-Blog/Dev-Blog/Models/Interfaces/IImage.cs
+++ Dev-Blog/Dev-Blog/Models/Interfaces/IImage.cs
@@ -11,31 +11,6 @@ namespace ECommerce.Models.Interfaces
     public interface IImage
     {
         /// <summary>
-        /// Gets a specified container from Azure storage
-        /// </summary>
-        /// <param name="name">Name of container to get</param>
-        /// <returns>container</returns>
-        public Task<CloudBlobContainer> GetContainer(string name);
-
-        /// <summary>
-        /// Gets a specified BLOB from an azure container
-        /// </summary>
-        /// <param name="imageName">Name of specified image</param>
-        /// <param name="containerName">Name of specified container</param>
-        /// <returns>BLOB</returns>
-        public Task<CloudBlob> GetBlob(string imageName, string containerName);
-
-        /// <summary>
-        /// Uploads a BLOB to specified container in Azure storage
-        /// </summary>
-        /// <param name="containerName">Name of specified container</param>
-        /// <param name="fileName">Name of specified file</param>
-        /// <param name="image">Name of specified image</param>
-        /// <param name="contentType">Type of content being uploaded</param>
-        /// <returns>Successful completion of task</returns>
-        public Task UploadFile(string containerName, string fileName, byte[] image, string contentType);
-
-        /// <summary>
         /// Uploads an image to a dropbox account
         /// </summary>
         /// <param name="image">The image to upload</param>
