876f70ad59af86107ec7f364e5067355a0cfb960
Andrew Smith <andrewcs149@gmail.com>
2020-10-16 08:09:01 -0700

    Add voting properties.

--- Dev-Blog/Dev-Blog/Models/Post.cs
+++ Dev-Blog/Dev-Blog/Models/Post.cs
@@ -12,6 +12,8 @@ namespace Dev_Blog.Models
         public DateTime Date { get; set; }
         public string ImgURL { get; set; }
         public string Description { get; set; }
+        public int UpVotes { get; set; }
+        public int DownVotes { get; set; }
         public List<Comment> Comments { get; set; }
     }
 }
