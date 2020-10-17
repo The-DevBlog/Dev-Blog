f5626e162680d52505d7e96e3e33f8dc5ff3be9f
Andrew Smith <andrewcs149@gmail.com>
2020-10-17 08:56:29 -0700

    Minor change.

---
+++ Dev-Blog/Dev-Blog/Models/Interfaces/IVote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Blog.Models.Interfaces
+{
+    public interface IVote
+    {
+        public Task Create(Vote vote);
+
+        public Task Delete(Vote vote);
+    }
+}
---
+++ Dev-Blog/Dev-Blog/Models/Services/VoteService.cs
@@ -0,0 +1,32 @@
+﻿using Dev_Blog.Data;
+using Dev_Blog.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Blog.Models.Services
+{
+    public class VoteService : IVote
+    {
+        private AppDbContext _context;
+
+        public VoteService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Vote vote)
+        {
+            _context.Entry(vote).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Vote vote)
+        {
+            _context.Entry(vote).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
