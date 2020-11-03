9881b4ba43bd9e97c94f90d184ba7016c194694d
Andrew Smith <andrewcs149@gmail.com>
2020-11-03 10:46:17 -0800

    Minor change.

--- Dev-Blog/Dev-Blog/Models/Services/EmailService.cs
+++ Dev-Blog/Dev-Blog/Models/Services/EmailService.cs
@@ -6,6 +6,8 @@ using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Dev_Blog.Models.Services
