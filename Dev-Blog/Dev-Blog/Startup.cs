9bbd344df259694c8dc2040460909d0fc36a4b18
Andrew Smith <andrewcs149@gmail.com>
2020-11-20 19:57:49 -0800

    Add CORS middleware.

--- Dev-Blog/Dev-Blog/Startup.cs
+++ Dev-Blog/Dev-Blog/Startup.cs
@@ -79,6 +79,11 @@ namespace Dev_Blog
                 app.UseHsts();
             }
 
+            app.UseCors(builder =>
+                builder.WithOrigins("https://thedevblog.azurewebsites.net/")
+                    .AllowAnyHeader()
+            );
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
