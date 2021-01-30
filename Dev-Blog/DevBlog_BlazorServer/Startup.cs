e130505fa23af73026c6ed16dafc921b99872ace
Andrew Smith <andrewcs149@gmail.com>
2021-01-30 12:53:30 -0800

    Style Navbar.

--- Dev-Blog/DevBlog_BlazorServer/Startup.cs
+++ Dev-Blog/DevBlog_BlazorServer/Startup.cs
@@ -28,6 +28,7 @@ namespace DevBlog_BlazorServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
