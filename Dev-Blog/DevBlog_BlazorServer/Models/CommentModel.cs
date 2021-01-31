6d61454a7bb121924b050259eef601d46668fc84
Andrew Smith <andrewcs149@gmail.com>
2021-01-31 14:22:16 -0800

    Add comment modal.

---
+++ Dev-Blog/DevBlog_BlazorServer/Models/CommentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBlog_BlazorServer.Models
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; private set; }
+        public string UserName { get; set; }
+
+        public CommentModel()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
