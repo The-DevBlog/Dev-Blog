178bfa0b5ca07b6df71d61e15cd790554ad61261
Andrew Smith <andrewcs149@gmail.com>
2021-02-01 13:17:30 -0800

    Add Vote model.

---
+++ Dev-Blog/DevBlog_BlazorServer/Models/VoteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevBlog_BlazorServer.Models
+{
+    public class VoteModel
+    {
+        public int PostId { get; set; }
+        public string UserId { get; set; }
+        public bool UpVote { get; set; }
+        public bool DownVote { get; set; }
+        public PostModel Post { get; set; }
+    }
+}
