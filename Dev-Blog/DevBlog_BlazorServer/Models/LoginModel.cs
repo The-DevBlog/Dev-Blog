b1e7960fe3eda6d7122b3a81a48f732e959c1a30
Andrew Smith <andrewcs149@gmail.com>
2021-02-03 10:41:04 -0800

    Minor change.

--- Dev-Blog/DevBlog_BlazorServer/Models/LoginModel.cs
+++ Dev-Blog/DevBlog_BlazorServer/Models/LoginModel.cs
@@ -9,9 +9,14 @@ namespace DevBlog_BlazorServer.Models
     public class LoginModel
     {
         [Required]
-        public string Email { get; set; }
+        [StringLength(25)]
+        public string UserName { get; set; }
+
+        //[Required]
+        //public string Email { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
