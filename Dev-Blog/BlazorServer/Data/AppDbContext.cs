b4def0c5ba0cb4b268e003c1716e73cc96e47f20
Andrew Smith <andrewcs149@gmail.com>
2021-02-10 16:25:58 -0800

    Add db context class.

--- Dev-Blog/BlazorServer/Data/AppDbContext.cs
+++ Dev-Blog/BlazorServer/Data/AppDbContext.cs
@@ -1,8 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BlazorServer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServer.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<PostModel> Post { get; set; }
+        public DbSet<CommentModel> Comment { get; set; }
+        public DbSet<VoteModel> Vote { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<VoteModel>(entity =>
+            {
+                entity.HasKey(e => new { e.PostId, e.UserId });
+            });
+        }
     }
 }
