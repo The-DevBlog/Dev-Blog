3d94e4a3eb7130df3c737491ee9ee7c9fa62eeac
Andrew Smith <andrewcs149@gmail.com>
2021-02-14 16:41:56 -0800

    Add new post email.

--- Dev-Blog/BlazorServer/Interfaces/IEmails.cs
+++ Dev-Blog/BlazorServer/Interfaces/IEmails.cs
@@ -5,5 +5,7 @@ namespace BlazorServer.Interfaces
     public interface IEmails
     {
         Task Welcome(string email);
+
+        Task NewPost(string img = null);
     }
 }
--- Dev-Blog/BlazorServer/Services/EmailService.cs
+++ Dev-Blog/BlazorServer/Services/EmailService.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,6 +40,34 @@ namespace BlazorServer.Services
             msg.AddTo(email);
 
             await client.SendEmailAsync(msg);
+        }
+
+        /// <summary>
+        /// Sends an email to all subscribed users when a new post is made
+        /// </summary>
+        /// <param name="img">The image to attach</param>"
+        /// <returns>Successful completion of task</returns>
+        public async Task NewPost(string img = null)
+        {
+            var apiKey = _config.GetSection("SENDGRID_APIKEY").Value;
+            var client = new SendGridClient(apiKey);
+
+            var msg = new SendGridMessage()
+            {
+                TemplateId = _config.GetSection("NEW_POST_EMAIL").Value,
+                From = new EmailAddress(_config.GetSection("AdminEmail").Value)
+            };
+
+            // get all users who are subscribed
+            List<UserModel> users = userMgr.Users.Where(x => x.UserName != _config.GetSection("AdminUserName").Value && x.Subscribed).ToList();
+
+            if (users.Count > 0)
+            {
+                List<EmailAddress> emails = new List<EmailAddress>();
+                users.ForEach(x => emails.Add(new EmailAddress(x.Email)));
+                msg.AddTos(emails);
+                await client.SendEmailAsync(msg);
+            }
         }
     }
 }
