afbe0724ee3efc27ab53e231a207b11d95f4074f
Andrew Smith <andrewcs149@gmail.com>
2021-04-05 10:41:07 -0700

    Minor changes.

---
+++ Dev-Blog/BlazorServer/Migrations/20210404180034_newTables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServer.Migrations
+{
+    public partial class newTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
