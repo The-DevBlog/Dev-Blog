e569000a63f24b3141e0dc6f372567dbea4652dd
Andrew Smith <andrewcs149@gmail.com>
2021-04-06 11:10:35 -0700

    Add Migrations.

---
+++ Dev-Blog/BlazorServer/Migrations/20210406180124_newmigration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServer.Migrations
+{
+    public partial class newmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ Dev-Blog/BlazorServer/Migrations/UserDb/20210406180138_newmigration2.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServer.Migrations.UserDb
+{
+    public partial class newmigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
