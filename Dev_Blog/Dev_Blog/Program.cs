9dc2400e5299d95ececdfad15e188520ed0e24a0
Andrew Smith <andrewcs149@gmail.com>
2022-01-16 18:23:33 -0800

    Minor fix.

--- Dev_Blog/Dev_Blog/Program.cs
+++ Dev_Blog/Dev_Blog/Program.cs
@@ -1,20 +1,73 @@
+using Dev_Blog.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Dev_Blog.Models;
+using Dev_Blog.Interfaces;
+using Dev_Blog.Services;
+using Blazored.Modal;
+using Dev_Blog.State;
+using System;
 
-namespace Dev_Blog
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddBlazoredModal();
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+builder.Services.AddScoped<IVoteRepository, VoteRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<AppState>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.UseMySql(Environment.GetEnvironmentVariable("DEVBLOG_DB_CON_STR"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+    options.UseMySql(Environment.GetEnvironmentVariable("DEVBLOG_USER_DB_CON_STR"));
+});
+
+builder.Services.AddIdentity<UserModel, IdentityRole>()
+        .AddEntityFrameworkStores<UserDbContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireRole(RoleModel.Admin));
+    options.AddPolicy("Visitor", policy => policy.RequireRole(RoleModel.Visitor));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+    endpoints.MapControllers();
+});
