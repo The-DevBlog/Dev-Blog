cd2500a788889e93e0aaee77483c3251d4b0c16a
andrew <andrewcs149@gmail.com>
2023-02-17 17:15:38 -0800

    add models

--- devblog/devblog/Models/PostModel.cs
+++ devblog/devblog/Models/PostModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace Dev_Blog.Models
 {
@@ -8,17 +7,18 @@ namespace Dev_Blog.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "update number required")]
         public string UpdateNum { get; set; }
 
-        [Required(ErrorMessage = "description required")]
         public string Description { get; set; }
 
         public DateTime Date { get; set; }
+
         public string ImgURL { get; set; }
 
         public List<UpVoteModel> UpVotes { get; set; }
+
         public List<DownVoteModel> DownVotes { get; set; }
+
         public List<CommentModel> Comments { get; set; }
     }
 }
