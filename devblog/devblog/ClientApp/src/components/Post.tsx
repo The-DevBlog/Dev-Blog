dc94a8d087021585ff4719703866816df74d60d0
andrew <andrewcs149@gmail.com>
2023-02-17 20:02:38 -0800

    Add styling

--- devblog/devblog/ClientApp/src/components/Post.tsx
+++ devblog/devblog/ClientApp/src/components/Post.tsx
@@ -1,10 +1,13 @@
 import IPost from "../interfaces/IPost";
+import "./Post.css";
 
 const Post = (props: IPost) => {
     return (
-        <div>
-            <span>{props.UpdateNum}</span>
-            <span>{props.Date}</span>
+        <div className="post">
+            <div className="post-info">
+                <span>{props.UpdateNum}</span>
+                <span>{props.Date}</span>
+            </div>
             <img style={{ display: "block" }} src={props.ImgURL} alt="image of post" />
             <p>{props.Description}</p>
         </div>
--- devblog/devblog/ClientApp/src/pages/Posts.tsx
+++ devblog/devblog/ClientApp/src/pages/Posts.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import IPost from "../interfaces/IPost";
 import Post from "../components/Post";
+import "./Posts.css";
 
 const Posts = () => {
     const [posts, setPosts] = useState<IPost[]>([]);
@@ -19,9 +20,11 @@ const Posts = () => {
     return (
         <>
             <h1>POSTS</h1>
-            {posts.map((p) => {
-                return <Post Id={p.Id} UpdateNum={p.UpdateNum} Date={p.Date} Description={p.Description} ImgURL={p.ImgURL} />
-            })}
+            <section className="posts-container">
+                {posts.map((p) => {
+                    return <Post Id={p.Id} UpdateNum={p.UpdateNum} Date={p.Date} Description={p.Description} ImgURL={p.ImgURL} />
+                })}
+            </section>
         </>
     );
 
