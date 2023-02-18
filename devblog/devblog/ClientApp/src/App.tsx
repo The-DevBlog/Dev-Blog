04823fb1d5c545d973c335df69ce670a1438949a
andrew <andrewcs149@gmail.com>
2023-02-17 17:13:25 -0800

    Add post model

--- devblog/devblog/ClientApp/src/App.tsx
+++ devblog/devblog/ClientApp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Nav from "./components/Nav";
 import Home from './pages/Home';
