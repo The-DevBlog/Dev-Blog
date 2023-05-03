e5b14a16e02dc2b64987cbcd344293534f707da3
andrew <andrewcs149@gmail.com>
2023-05-03 01:07:06 -0700

    remove unsued files

--- Dev_Blog/Dev_Blog/Models/RoleModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dev_Blog.Models
-{
-    public class RoleModel
-    {
-        public const string Admin = "Admin";
-        public const string Visitor = "Visitor";
-    }
-}
--- Dev_Blog/Dev_Blog/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Dev_Blog.Models
-{
-    public class UserModel : IdentityUser
-    {
-        public bool Subscribed { get; set; }
-    }
-}
