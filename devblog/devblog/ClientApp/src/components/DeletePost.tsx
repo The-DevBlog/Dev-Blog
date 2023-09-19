56d0656254e28f29efa681987151c2eb76f6bcea
andrew <andrewcs149@gmail.com>
2023-09-18 17:45:13 -0700

    nav to home page after deleting a post

--- devblog/devblog/ClientApp/src/components/DeletePost.tsx
+++ devblog/devblog/ClientApp/src/components/DeletePost.tsx
@@ -1,11 +1,13 @@
 import IPost from "../interfaces/IPostProps";
 import { MdDelete as Trash } from "react-icons/md";
 import { GetIsAdmin } from "../components/AuthenticationService";
+import { useNavigate } from "react-router-dom";
 import "./styles/DeletePost.css";
 import { useEffect, useState } from "react";
 
 const DeletePost = (props: IPost) => {
     const [isAdmin, setIsAdmin] = useState(false);
+    const navigate = useNavigate();
 
     const handleDelete = () => {
         fetch(`api/posts/${props.id}`, {
@@ -14,6 +16,8 @@ const DeletePost = (props: IPost) => {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             }
+        }).then(() => {
+            navigate("/");
         });
     };
 
